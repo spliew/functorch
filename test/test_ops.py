942f1d8c0e2f1a8df99a0eb638e9b352355f13e1
samdow <samdow@fb.com>
2022-06-02 10:01:28 -0400

    add exhaustive testing of vmap autograd composability

--- test/test_ops.py
+++ test/test_ops.py
@@ -200,28 +200,31 @@ def get_vjp_fn_and_args_with_cotangents(f, sample, cotangents):
 def get_autograd_fn_and_args_with_cotangents(f, sample, cotangents):
     args = tuple([sample.input] + list(sample.args))
     kwargs = sample.kwargs
-    flat_args, args_spec = tree_flatten(args)
+    flat_args, _ = tree_flatten(args)
     flat_cotangents, cotangents_spec = tree_flatten(cotangents)
 
     @functools.wraps(f)
-    def wrapped(*args):
-        assert len(args) == len(flat_args) + len(flat_cotangents)
-        actual_args = args[:len(flat_args)]
-        cotangents = args[len(flat_args):]
-        actual_args = tree_unflatten(actual_args, args_spec)
-        cotangents = tree_unflatten(cotangents, cotangents_spec)
+    def wrapped(*cotangents):
+        def is_differentiable(out):
+            return isinstance(out, Tensor) and (out.grad_fn is not None or out.requires_grad)
 
-        fn, primals = normalize_op_input_output3(f, actual_args, kwargs,
+        assert len(cotangents) == len(flat_cotangents)
+
+        fn, primals = normalize_op_input_output3(f, args, kwargs,
                                                  flat_args,
                                                  sample.output_process_fn_grad)
         out = fn(*primals)
-        diff_wrt = tuple(primal for primal in primals if (primal.requires_grad or primal.grad_fn is not None))
-        if diff_wrt:
-            return torch.autograd.grad(out, diff_wrt, grad_outputs=cotangents)
-        else:
-            return (torch.ones(()),)  # uuugh hack...this will need to be more generic
+        if not isinstance(out, tuple):
+            out = (out,)
+        filtered_primals = [primal for primal in primals if primal.requires_grad]
+        flattened_out, _ = tree_flatten(out)
+        out_cotangent = zip(out, cotangents)
+        filtered_out_cotangent = [(out, cotangent) for (out, cotangent) in out_cotangent if is_differentiable(out)]
+        filtered_out, filtered_cotangents = zip(*filtered_out_cotangent)
+        grads = torch.autograd.grad(filtered_out, filtered_primals, grad_outputs=cotangents, allow_unused=True)
+        return [grad for grad in grads if grad is not None]
 
-    return wrapped, tuple(flat_args + flat_cotangents)
+    return wrapped, flat_cotangents
 
 
 # Returns a new function g(*args, *cotangents) that computes vjps and
@@ -1422,21 +1425,49 @@ class TestOperators(TestCase):
                 cotangents = torch.randn_like(result, device=device)
                 self._compare_jacobians_of_vjp(fn, (cotangents, input, weight, bias))
 
-    @ops(filter(lambda op: op.name == "nn.functional.group_norm", functorch_lagging_op_db + additional_op_db),
-         allowed_dtypes=(torch.float32, torch.double))  # TODO: generalize
-    def test_group_norm_backward(self, device, dtype, op):
-        # hacky, only works since no group norm inputs can be scalars
-        def was_skipped_from_batched_tensors(batched_out, batch_size):
-            return batched_out.shape == (batch_size,) and all(tuple(e == 1 for e in batched_out))
+    @skipOps('TestOperators', 'test_vmap_autograd_grad', {
+        # randomness errors
+        xfail('bernoulli'),
+        xfail('nn.functional.dropout2d'),
+        xfail('nn.functional.dropout'),
+        xfail('nn.functional.feature_alpha_dropout', 'with_train'),
+        xfail('nn.functional.fractional_max_pool2d'),
+        xfail('nn.functional.fractional_max_pool3d'),
+        xfail('nn.functional.rrelu'),
+        xfail('normal'),
+        xfail('normal', 'number_mean'),
+        xfail('pca_lowrank'),
+        xfail('svd_lowrank'),
+
+        # call inplace functions
+        xfail('linalg.eigh'),  # inplace
+        xfail('linalg.householder_product'),  # inplace
+        xfail('matrix_exp'),  # inplace
+        xfail('symeig'),  # inplace
+        xfail('take'),  # inplace
+
+        xfail('linalg.eig'),  # all close?
+        # The size of tensor a (4) must match the size of tensor b (10) at non-singleton dimension 0
+        xfail('masked_select'),
+        xfail('nn.functional.max_unpool2d', 'grad'),  # contiguous call
+        xfail('nn.functional.max_unpool2d'),  # contiguous call
+        xfail('to_sparse'),  # dispatch key issue
+
+        # numerical inconsistencies, look like bugs
+        xfail('nn.functional.binary_cross_entropy_with_logits', dtypes=(torch.float32,)),
+        xfail('ldexp', dtypes=(torch.float32,)),
+    })
+    @ops(functorch_lagging_op_db + additional_op_db, allowed_dtypes=(torch.float32, torch.double))
+    def test_vmap_autograd_grad(self, device, dtype, op):
+        if not op.supports_autograd:
+            self.skipTest("Skipped! Autograd not supported.")
+            return
 
         sample_inputs = op.sample_inputs(device, dtype, requires_grad=True)
-
         for sample_input in sample_inputs:
             cotangents = get_sample_cotangents(op, sample_input)
             f, args = get_autograd_fn_and_args_with_cotangents(op, sample_input, cotangents)
             for loop_out, batched_out in get_fallback_and_vmap_exhaustive(f, args, {}, opinfo=op):
-                if all(was_skipped_from_batched_tensors(bo, lo.shape[0]) for (bo, lo) in zip(batched_out, loop_out)):
-                    continue  # we weren't able to use the batched tensor in autograd.grad
                 self.assertEqual(loop_out, batched_out)
 
 
